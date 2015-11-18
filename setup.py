8bdf4a6891265fe1a15c8572dab32c325d5333ab
Alec <alec@tinycountry.com>
2015-11-18 11:13:57 -0800

    add author info

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
 setup(name='sitemap_loader',
+    author='Alec Deason',
+    email='alec@brandverity.com',
     version='1.0',
     install_requires=['tldextract'],
     scripts=['sitemap_loader']
